9301badbc28b35434e19cbdacb9e722c90bace4b
Ivan Mikodanic <ivan.mikodanic@rimac-technology.com>
2023-04-13 15:59:59 +0200

    feat(root): setup treblle and a hello world request

--- src/app.js
+++ src/app.js
@@ -1,10 +1,24 @@
 const express = require('express')
+const treblle = require('@treblle/express')
 
 // Initialize express app
 const app = express()
 
 // Parse json request body
 app.use(express.json())
+
+// Pass treblle middleware to express app
+app.use(
+	treblle({
+		apiKey: process.env.TREBLLE_API_KEY,
+		projectId: process.env.TREBLLE_PROJECT_ID,
+		additionalFieldsToMask: [],
+	})
+)
+
+app.get('/', (req, res) => {
+	res.json({ test: 'Hello World!' })
+})
 
 // Export it so index.js can use it to start the server
 module.exports = app
