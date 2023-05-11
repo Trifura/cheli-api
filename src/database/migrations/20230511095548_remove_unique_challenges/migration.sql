6e6568dd61b086bbbeb0c0ccefea3b4c7c628b18
Ivan Mikodanic <ivan.mikodanic@rimac-technology.com>
2023-05-11 12:02:48 +0200

    feat: add challenge icons

---
+++ src/database/migrations/20230511095548_remove_unique_challenges/migration.sql
@@ -0,0 +1,8 @@
+-- DropIndex
+DROP INDEX `challenges_icon_key` ON `challenges`;
+
+-- DropIndex
+DROP INDEX `challenges_image_key` ON `challenges`;
+
+-- DropIndex
+DROP INDEX `challenges_sponsor_key` ON `challenges`;
--- src/database/seed/challenges.seed.ts
+++ src/database/seed/challenges.seed.ts
@@ -11,12 +11,14 @@ const challenges: Challenge[] = [
 		uuid: uuidv4(),
 		title: 'Do 5 pushups',
 		description: 'In order to complete the challenge you must do 5 pushups',
+		icon: '💪',
 	},
 	{
 		uuid: uuidv4(),
 		title: 'Give a compliment to a stranger',
 		description:
 			'You have to give a compliment to a random person on the streets',
+		icon: '👋',
 	},
 ]
 
@@ -24,7 +26,7 @@ const main = async () => {
 	for (const challenge of challenges) {
 		await prisma.challenge.upsert({
 			where: { title: challenge.title },
-			update: {},
+			update: { ...challenge },
 			create: challenge,
 		})
 	}
