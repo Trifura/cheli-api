5bb06b074ca17e5703f850b3d3349260a3137e1f
Ivan Mikodanic <ivan.mikodanic@rimac-technology.com>
2023-05-19 18:18:03 +0200

    feat: initial setup

--- src/controllers/index.ts
+++
@@ -1,5 +0,0 @@
-import AuthController from './auth.controller'
-import ChallengeController from './challenge.controller'
-import UserController from './user.controller'
-
-export { AuthController, ChallengeController, UserController }
--- src/database/migrations/20230511095548_remove_unique_challenges/migration.sql
+++
@@ -1,8 +0,0 @@
--- DropIndex
-DROP INDEX `challenges_icon_key` ON `challenges`;
-
--- DropIndex
-DROP INDEX `challenges_image_key` ON `challenges`;
-
--- DropIndex
-DROP INDEX `challenges_sponsor_key` ON `challenges`;
