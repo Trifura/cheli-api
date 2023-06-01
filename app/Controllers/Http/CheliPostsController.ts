efae98aefb7bf15075a2eedfeb64f84dbbbcdb19
Ivan Mikodanic <ivan.mikodanic@rimac-technology.com>
2023-06-01 20:16:32 +0200

    feat: on complete check if cheli post expired +

--- app/Controllers/Http/CheliPostsController.ts
+++ app/Controllers/Http/CheliPostsController.ts
@@ -1,16 +1,23 @@
 import type { HttpContextContract } from '@ioc:Adonis/Core/HttpContext'
 import CheliPost from 'App/Models/CheliPost'
 import CheliPostLike from 'App/Models/CheliPostLike'
+import { DateTime } from 'luxon'
 
 export default class CheliPostsController {
   public async completeCheliPost({ request, response }: HttpContextContract) {
     const { cheliPostId } = request.params()
 
-    // TODO: check if cheli post expired
-
     const cheliPost = await CheliPost.query().where('id', cheliPostId).preload('cheli').first()
 
     if (!cheliPost) return response.status(404).json({ message: 'Cheli post not found' })
+
+    const oneDay = 24 * 60 * 60 * 1000
+
+    const cheliDeadline = cheliPost?.createdAt?.toMillis() + oneDay
+
+    if (DateTime.now().toMillis() >= cheliDeadline) {
+      return response.status(400).json({ message: 'Cheli post expired' })
+    }
 
     cheliPost.isCompleted = true
     const check = await cheliPost.save()
